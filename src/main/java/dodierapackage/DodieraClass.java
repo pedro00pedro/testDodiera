17544e977bc380cb5f968829870a9156f4591863
pedro00pedro <pedro96vbc@gmail.com>
2021-10-02 23:31:03 -0300

    (╯°□°)╯︵ ┻━┻ 1st Commit

---
+++ src/main/java/dodierapackage/DodieraClass.java
@@ -0,0 +1,11 @@
+package dodierapackage;
+
+public class DodieraClass {
+
+   public String ameba = "Stringzin";
+
+    public static void main(String[] args) {
+        System.out.println("Hello Dodiera");
+    }
+
+}
---
+++ src/test/java/dodierapackage/DodieraClassTest.java
@@ -0,0 +1,20 @@
+package dodierapackage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DodieraClassTest {
+
+
+
+    @Test
+    public void amebaTest(){
+        System.out.println("Dodiera Teste 123");
+        DodieraClass dc = new DodieraClass();
+
+        throw new IllegalStateException("");
+
+        //assertEquals("DEU RUIM", "Stringzinha", dc.ameba);
+    }
+}
